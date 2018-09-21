2c2cc0ec6d2d474cc41a4e44bc881e2cc0f1f439
NKb03 <niko.knop003@gmail.com>
2018-09-21 10:51:04 +0200

    Made mutableXXX return MutableRefs

--- src/main/kotlin/org/nikok/kref/API.kt
+++ src/main/kotlin/org/nikok/kref/API.kt
@@ -4,6 +4,8 @@
 
 package org.nikok.kref
 
+import org.nikok.kref.impl.MutableSoftRef
+import org.nikok.kref.impl.MutableWeakRef
 import org.nikok.kref.impl.SoftRef
 import org.nikok.kref.impl.WeakRef
 import java.lang.ref.SoftReference
@@ -29,19 +31,19 @@ fun <T> strong(referent: T): Ref<T> = SoftRef(referent)
 /**
  * @return a property delegate holding a [WeakReference] to [referent]
  */
-fun <T> mutableWeak(referent: T): Ref<T> = WeakRef(referent)
+fun <T> mutableWeak(referent: T): MutableRef<T> = MutableWeakRef(referent)
 
 /**
  * @return a property delegate holding a [SoftReference] to [referent]
  */
 
-fun <T> mutableSoft(referent: T): Ref<T> = SoftRef(referent)
+fun <T> mutableSoft(referent: T): MutableRef<T> = MutableSoftRef(referent)
 
 /**
  * @return a property delegate holding a strong reference to [referent]
  */
 
-fun <T> mutableStrong(referent: T): Ref<T> = SoftRef(referent)
+fun <T> mutableStrong(referent: T): MutableRef<T> = MutableSoftRef(referent)
 
 /**
  * @return a [RefWrapper] wrapping [ref]
